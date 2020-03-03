14766523ba3790886b81935d3d4dc12c89ac7df8
Michiel Leunens <michiel.leunens@gmail.com>
2020-03-03 13:29:48 +0100

    Config of the website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,33 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `LoWatter`,
+    description: `Controlling legionella in tapwater.`,
+    author: `@michielleunens`,
+    siteUrl: `https://www.lowatter.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/static/uploads`,
+        name: "uploads",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdown`,
+        name: `markdown`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -16,19 +38,78 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-transformer-remark`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images`,
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 2048,
+            },
+          },
+        ],
       },
     },
+    {
+      resolve: `gatsby-plugin-intl`,
+      options: {
+        path: `${__dirname}/src/utils/locales`,
+        languages: [`en`, `nl`],
+        defaultLanguage: `nl`,
+        redirect: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `LoWatter`,
+        short_name: `LoWatter`,
+        description: `Controlling legionella in tapwater.`,
+        start_url: `/nl/`,
+        lang: `nl`,
+        background_color: `#4DD996`,
+        theme_color: `#4DD996`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+        localize: [
+          {
+            start_url: `/en/`,
+            lang: `en`,
+            name: `LoWatter`,
+            short_name: `LoWatter`,
+            description: `Beheren van legionella in kraantjeswater.`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-lodash`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /\.inline\.svg$/,
+        },
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify-cache`,
   ],
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,23 @@
+import Typography from "typography"
+const typography = new Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.6,
+  headerFontFamily: ["Anton", "serif"],
+  bodyFontFamily: ["Oswald", "sans-serif"],
+  headerColor: "#2D302D",
+  bodyColor: "#616765",
+  googleFonts: [
+    {
+      name: "Oswald",
+      styles: ["400", "600"],
+    },
+    {
+      name: "Anton",
+      styles: ["400"],
+    },
+  ],
+})
+
+typography.injectStyles()
+
+export default typography
