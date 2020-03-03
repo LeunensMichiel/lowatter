44db327612c829ee77d07ce8a93642b723614af5
Michiel Leunens <michiel.leunens@gmail.com>
2020-03-03 16:32:26 +0100

    Initial Site

---
+++ src/components/framework/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+  black: "#2D302D",
+  darkAccent: "#164449",
+  white: "#FAFAFF",
+  accent: "#4BB98B",
+  accent2: "#4DD996",
+  secondaryBlack: "#616765",
+  gradient: "linear-gradient(102.48deg, #4BB98B 9.06%, #4DD996 90.94%)",
+  purplegradient: "linear-gradient(180deg, #F092FF 0%, #662871 100%)",
+  water: "#C8EDFF",
+}
+
+export default colors
