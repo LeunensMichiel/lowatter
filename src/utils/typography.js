a2e1f51612443ce55696b49c49a9154bdec3d7b5
Michiel Leunens <michiel.leunens@gmail.com>
2020-05-07 12:41:21 +0200

    More fonts fallback

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,11 +1,32 @@
 import Typography from "typography"
 const typography = new Typography({
   baseFontSize: "16px",
-  baseLineHeight: 1.5,
-  headerFontFamily: ["Open Sans", "sans-serif"],
-  bodyFontFamily: ["Oswald", "sans-serif"],
+  baseLineHeight: 1.6,
+  headerFontFamily: [
+    "Open Sans",
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Ubuntu",
+    "Cantarell",
+    "Helvetica Neue",
+    "sans-serif",
+  ],
+  bodyFontFamily: [
+    "Oswald",
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Ubuntu",
+    "Cantarell",
+    "Helvetica Neue",
+    "sans-serif",
+  ],
   headerColor: "#2D302D",
   bodyColor: "#616765",
+  bodyWeight: "400",
   omitGoogleFont: true,
 })
 
